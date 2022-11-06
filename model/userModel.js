b326eb2b01de5eae19c5e11b1279c3db196f5a2c
PoojaMoukhede <poojamoukhede27@gmail.com>
2022-11-06 14:35:12 +0530

    router added

---
+++ model/userModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const Schema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: { type: String },
+});
+
+const user = mongoose.model("User", Schema);
+module.exports = user;
